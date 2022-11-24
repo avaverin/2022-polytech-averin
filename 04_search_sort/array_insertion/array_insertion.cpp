daaf815246eb51a7fa48d071ee80962b229e6247
avaverin <averin.av@edu.spbstu.ru>
2022-11-25 00:36:57 +0300

    fix: new program

--- 04_search_sort/array_insertion/array_insertion.cpp
+++ 04_search_sort/array_insertion/array_insertion.cpp
@@ -1,60 +1,51 @@
 #include <cassert>
 
-int search_position(int arr[], int n, int x) 
-{
-  for (auto i = 0; i < n - 1; ++i) 
-  {
-    if (arr[i] >= x)
-      return i;
-  }
-  return n;
-}
+int* insert(int arr[], int n, int x, int p);
+int search_position(int arr[], int n, int x);
+int* insert_sorted(int arr[], int n, int x);
 
+int main() {
 
-int insert(int arr[], int n, int x, int p) {
-  for (auto i = n; i > p; i--)
-    arr[i] = arr[i - 1];
+    int a0[10] = { 0, 1, 2, 3,      4, 5, 6, 7, 8, 9 };
+    int expected_r0[11] = { 0, 1, 2, 3, 100, 4, 5, 6, 7, 8, 9 };
 
-  arr[p] = x;
-  return arr;
-}
-
-
-int insert_sorted(int arr[], int n, int x) {
-  int p = search_position(arr, n, x);
-  return insert(arr, n, x, p);
-};
-
-
-
-int main() 
-{
-    int a1[4] = {1, 3, 5, 6}; //search_position example
-    assert(search_position(a1, 4, 5) == 2);
-
-    int a2[4] = {1, 3, 5, 6};
-    assert(search_position(a2, 4, 7) == 4);
-
-
-  
-    int a0[3] ={1,5,8}; //// insert example
-    int expected_r0[4] = {1,4,5,8};
-
-    auto r0 = insert(a0, 3, 4, 1);
-    for (auto i = 3; i > 1; i--)
+    auto r0 = insert(a0, 10, 100, 4);
+    for (auto i = 0; i < 11; ++i)
         assert(r0[i] == expected_r0[i]);
 
+    int a1[4] = { 1, 3, 5, 6 };
+    assert(search_position(a1, 4, 5) == 2);
 
+    int a2[4] = { 1, 3, 5, 6 };
+    assert(search_position(a2, 4, 7) == 4);
 
-    int a4[10] =          {0, 1, 2, 3, 4, 5,    6, 7, 8, 9}; //// insert_sorted example
-    int expected_r4[11] = {0, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9};
+    int a4[10] = { 0, 1, 2, 3, 4, 5,    6, 7, 8, 9 };
+    int expected_r4[11] = { 0, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9 };
 
     auto r4 = insert_sorted(a4, 10, 5);
-    for (auto i = 10; i > 6; i--)
+    for (auto i = 0; i < 11; ++i)
         assert(r4[i] == expected_r4[i]);
 
     return 0;
 }
 
-    
-  
+int* insert(int arr[], int n, int x, int p) {
+    for (auto i = n; i > p; i--)
+        arr[i] = arr[i - 1];
+
+    arr[p] = x;
+    return arr;
+}
+
+int search_position(int arr[], int n, int x) {
+    for (auto i = 0; i < n - 1; ++i) {
+        if (arr[i] >= x)
+            return i;
+    }
+    return n;
+}
+
+int* insert_sorted(int arr[], int n, int x) {
+    int p = search_position(arr, n, x);
+    return insert(arr, n, x, p);
+}
