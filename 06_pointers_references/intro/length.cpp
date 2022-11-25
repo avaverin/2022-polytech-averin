23cea79c514388cdd6d5d3a45d5dbc218efdc3c0
avaverin <averin.av@edu.spbstu.ru>
2022-11-26 01:46:19 +0300

    feat:lenght function

---
+++ 06_pointers_references/intro/length.cpp
@@ -0,0 +1,28 @@
+#include <cassert>
+
+unsigned int length(const char* str)
+{
+    if (str == nullptr)
+    {
+        return 0;
+    }
+    else
+    {
+        int size = 0;
+        while (*str++)
+        {
+            size++;
+        }
+        return size;
+    }
+}
+
+int main()
+{
+    assert(length("a") == 1);
+    assert(length("ab") == 2);
+    assert(length("") == 0);
+    assert(length("hello world") == 11);
+    assert(length(nullptr) == 0);
+    return 0;
+}
